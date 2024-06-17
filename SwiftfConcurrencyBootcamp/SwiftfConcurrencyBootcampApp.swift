d4da746a79204c37529d532cd7bf21e73a6a7d73
Suppasit <monitorman_sc@hotmail.com>
2024-06-17 16:35:45 +0700

    Initial Commit

---
+++ SwiftfConcurrencyBootcamp/SwiftfConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfConcurrencyBootcampApp.swift
+//  SwiftfConcurrencyBootcamp
+//
+//  Created by Suppasit chuwatsawat on 17/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
