596ce50c0cf66dee3d200d51880fce5a0729d91c
Suppasit <monitorman_sc@hotmail.com>
2024-07-03 09:21:17 +0700

    How to use MVVM with Async Await

---
+++ SwiftfConcurrencyBootcamp/MVVMBootcamp.swift
@@ -0,0 +1,73 @@
+//
+//  MVVMBootcamp.swift
+//  SwiftfConcurrencyBootcamp
+//
+//  Created by Suppasit chuwatsawat on 3/7/2567 BE.
+//
+
+import SwiftUI
+
+final class MyManagerClass {
+    
+    func getData() async throws -> String {
+        "Some Data!"
+    }
+    
+}
+
+actor MyManagerActor {
+    
+    func getData() async throws -> String {
+        "Some Data!"
+    }
+
+}
+
+@MainActor
+final class MVVMBootcampViewModel: ObservableObject {
+    
+    let managerClass = MyManagerClass()
+    let managerActor = MyManagerActor()
+   
+    @Published private(set) var myData: String = "Starting text"
+    private var tasks: [Task<Void, Never>] = []
+    
+    func cancelTasks() {
+        tasks.forEach { $0.cancel() }
+        tasks = []
+    }
+    
+    // @MainActor
+    func onCallToActionButtonPressed() {
+        let task = Task {
+            do {
+                // myData = try await managerClass.getData()
+                myData = try await managerActor.getData()
+            } catch {
+                print(error)
+            }
+        }
+        tasks.append(task)
+    }
+    
+}
+
+struct MVVMBootcamp: View {
+    
+    @StateObject private var viewModel = MVVMBootcampViewModel()
+    
+    var body: some View {
+        VStack {
+            Button("Clock me") {
+                viewModel.onCallToActionButtonPressed()
+            }
+        }
+        .onDisappear {
+            
+        }
+    }
+}
+
+#Preview {
+    MVVMBootcamp()
+}
